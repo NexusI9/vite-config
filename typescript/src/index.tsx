cd6e611687c34d7961eb25d98b2de694fc11ba38
NexusI9 <nassim.elkhantour@gmail.com>
2024-03-06 16:32:05 +0800

    clean up ts config

--- typescript/src/index.tsx
+++ typescript/src/index.tsx
@@ -1,15 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
-import { Provider } from 'react-redux';
-import store from '@lib/store';
-
 document.addEventListener('DOMContentLoaded', function () {
-  const container = document.getElementById('stylee-app');
+  const container = document.getElementById('app');
   const root = container && createRoot(container);
   root?.render(
-    <Provider store={store}>
       <App />
-    </Provider>
   );
 });
